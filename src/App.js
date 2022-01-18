a2effd10ddcd821ba7455588d672efb744c62c49
Aurora2021 <aurorawangca@gmail.com>
2022-01-18 16:45:51 -0700

    added text to homepage

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div>
       <h1 style={{textAlign: 'center'}}>Hello, Sparkle Star.</h1>
-      <h2>Nice to meet you!</h2>
+      <h2 style={{textAlign: 'center'}}>Nice to meet you!</h2>
     </div>
   );
 }
